ab7ed0e61a8b21616289df4350a445fd4848803f
bl4ckm45k <nonpowa@gmail.com>
2022-08-11 02:26:10 +0300

    Пример параметров корзины для версии v1.0.*

--- examples/basket_params.py
+++ examples/basket_params.py
@@ -1,27 +1,25 @@
 import asyncio
 import logging
 
-from loader import api_client
+from loader import api
 
 logger = logging.getLogger(__name__)
 
 
 async def get_basket_params():
-    options = await api_client.basket_options()
-    payment = await api_client.basket_payment_method()
-    shipment = await api_client.basket_shipment_method()
-    addresses = await api_client.basket_shipment_address()
-    offices = await api_client.basket_shipment_offices()
-    logger.info(f'\n{options}\n{payment}\n{shipment}\n{addresses}\n{offices}')
+    options = await api.cp.client.basket.options()
+    payment = await api.cp.client.basket.payment_method()
+    shipment = await api.cp.client.basket.shipment_method()
+    addresses = await api.cp.client.basket.shipment_address()
+    offices = await api.cp.client.basket.shipment_offices()
+    logger.info(f'{options}\n{payment}\n{shipment}\n{addresses}\n{offices}')
 
     # Прочтите аннотации к методу перед использованием
-    await api_client.set_client_params(
-        payment_method_index=0,
-        shipment_address_index=0,
-        shipment_office_index=0
-    )
-    orders_list = await api_client.get_orders_list(orders=[94233131,
-                                                    93745568])
+    await api.cp.client.basket.set_client_params(payment_method_index=0,
+                                                 shipment_address_index=0,
+                                                 shipment_office_index=0)
+    orders_list = api.cp.client.orders.orders_list(orders=[94233131,
+                                                           93745568])
     logger.info(f'{orders_list}')
 
 
