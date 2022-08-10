b5931d3f4f4c5c43a4bb29be4b7d62908758d377
bl4ckm45k <nonpowa@gmail.com>
2022-08-11 02:16:11 +0300

    Пример поисковых запросов, v1.0.*

--- examples/search_methods.py
+++ examples/search_methods.py
@@ -1,38 +1,43 @@
 import asyncio
 import logging
 
-from loader import api, api_client
+from loader import api
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 async def search_brands():
-    data = await api_client.search_brands(602000800)
+    data = await api.cp.client.search.brands(602000800)
     logger.info(data)
-    await api_client.close()
+    await api.close()
 
 
 async def search_articles():
-    data = await api_client.search_articles(602000600, 'Luk', 1, 1, 1)
+    data = await api.cp.client.search.articles(602000600, 'Luk',
+                                               use_online_stocks=True,
+                                               disable_online_filtering=True,
+                                               with_out_analogs=True
+                                               )
     logger.info(data)
-    await api_client.close()
+    await api.close()
 
 
 async def search_bach():
     list_to_search = [{'brand': 'LuK', 'number': '602000600'}, {'brand': 'Mahle', 'number': 'OX8236D'}]
-    data = await api_client.search_bach(list_to_search)
+    data = await api.cp.client.search.batch(list_to_search)
     logger.info(data)
 
 
 async def search_history():
-    logger.info(f'{await api_client.search_history()}')
+    logger.info(f'{await api.cp.client.search.history()}')
 
 
 async def advices_batch():
     positions = [{"brand": "kyb", "number": "331009"}, {"brand": "Mobil", "number": "152566"}]
-    data = await api_client.advices_batch(positions)
+    data = await api.cp.client.search.advices_batch(positions)
     logger.info(data)
+
 
 if __name__ == '__main__':
     try:
