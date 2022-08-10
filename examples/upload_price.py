aa47e6d7147e9e11982bcfab2f857d2f54036dd6
bl4ckm45k <nonpowa@gmail.com>
2022-08-11 02:15:56 +0300

    Пример загрузки, v1.0.*

--- examples/upload_price.py
+++ examples/upload_price.py
@@ -6,20 +6,19 @@ from loader import api
 logger = logging.getLogger(__name__)
 
 
-async def example_upload_price_list(distributor_id, file_path):
-    data = await api.pricelist_update(distributor_id, file_path)
+async def example_upload_price_list(distributor_id, file_or_path):
+    data = await api.cp.admin.distributors.pricelist_update(distributor_id, file_or_path)
     logger.info(data)
 
 
 if __name__ == '__main__':
-    loop = asyncio.new_event_loop()
     import os
 
     cwd = os.getcwd()
     files = os.listdir('files')
     distributors = [1642117]
     for i in range(len(files)):
-        loop.run_until_complete(example_upload_price_list(distributor_id=distributors[i],
-                                                          file_path=open(f'{cwd}/files/{files[i]}', 'rb')))
-        # loop.run_until_complete(example_upload_price_list(distributor_id=1642117,
-        #                                                   file_path=f'{cwd}/files/{files[i]}'))
+        asyncio.run(example_upload_price_list(distributor_id=distributors[i],
+                                              file_or_path=open(f'{cwd}/files/{files[i]}', 'rb')))
+        # asyncio.run(example_upload_price_list(distributor_id=1642117,
+        #                                       file_or_path=f'{cwd}/files/{files[i]}'))
