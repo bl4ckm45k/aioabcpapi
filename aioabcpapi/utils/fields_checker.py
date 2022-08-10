4ba2ed29b10090080a9385d369ca7b667e0cb263
bl4ckm45k <nonpowa@gmail.com>
2022-08-11 02:17:37 +0300

    args fields, additional_info, statuses checker

---
+++ aioabcpapi/utils/fields_checker.py
@@ -0,0 +1,17 @@
+from aioabcpapi.exceptions import AbcpWrongParameterError
+
+
+def check_fields(fields_to_check, fields_values):
+    if isinstance(fields_to_check, str):
+        if fields_to_check not in fields_values:
+            raise AbcpWrongParameterError(
+                f'Параметр "fields" может принимать значения {fields_values}\n'
+                f'Для передачи нескольких параметров передавайте list')
+        else:
+            return fields_to_check
+    if isinstance(fields_to_check, list):
+        if all(x in fields_values for x in fields_to_check):
+            return ','.join(fields_to_check)
+        else:
+            raise AbcpWrongParameterError(
+                f'Параметр "fields" может принимать значения {fields_values}')
