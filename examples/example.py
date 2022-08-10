bb3de01c0e3b12bda5df98f750f01672bafba304
bl4ckm45k <nonpowa@gmail.com>
2022-08-11 02:18:03 +0300

    Пример из README, v1.0.*

--- examples/example.py
+++ examples/example.py
@@ -1,35 +1,35 @@
 import asyncio
 import datetime
 import logging
-# from aiohttp import web
 
-from loader import api
 from config import guest_id
+from loader import api, api_client
 
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-async def test_ur_request(update_start, update_end):
-    data = await api.get_orders_list(
-        date_updated_start=update_start,
-        date_updated_end=update_end,
-        format='additional')
+async def orders_list(update_start, update_end):
+    logger.info(f'{update_start}, {update_end}')
+    data = await api.cp.admin.orders.get_orders_list(date_updated_start=update_start,
+                                                     date_updated_end=update_end,
+                                                     format='additional')
+
     for x in data:
         if x['userId'] == guest_id:
-            logger.info(x['additional']['phone'], x['additional']['consumer'])
+            logger.info(f"{x['additional']['phone']}, {x['additional']['consumer']}")
     await api.close()
 
 
-# async def on_startup():
-#     app.add_routes([web.post(f'/web/some_path/cdek', cdek_webhook)])
+async def not_enough_rights(update_start, update_end):
+    data = await api_client.cp.admin.orders.get_orders_list(date_updated_start=update_start,
+                                                            date_updated_end=update_end)
+    logger.error(f'{data}')
+    await api_client.close()
 
 
 if __name__ == '__main__':
-    # app = web.Application()
-    loop = asyncio.new_event_loop()
-    date_start = (datetime.datetime.now() - datetime.timedelta(minutes=15)).strftime("%Y-%m-%d %H:%M:%S")
-    date_end = (datetime.datetime.now() - datetime.timedelta(minutes=2)).strftime("%Y-%m-%d %H:%M:%S")
-    loop.run_until_complete(test_ur_request(update_start=date_start, update_end=date_end))
-
-    # loop.run_until_complete(on_startup())
-    # web.run_app(app, host='127.0.0.1', port=5011)
+    date_start = datetime.datetime.now() - datetime.timedelta(days=3)
+    date_end = datetime.datetime.now()
+    asyncio.run(orders_list(update_start=date_start, update_end=date_end))
+    # asyncio.run(not_enough_rights(update_start=date_start, update_end=date_end))
