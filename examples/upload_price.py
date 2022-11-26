2b7e1d3e95bf2ba703ad0e2fdd49896a9ae2ac0c
bl4ckm45k <nonpowa@gmail.com>
2022-11-26 23:29:18 +0300

    Переименование аргумента в соответсвии с документацией

--- examples/upload_price.py
+++ examples/upload_price.py
@@ -6,10 +6,10 @@ from loader import api
 logger = logging.getLogger(__name__)
 
 
-async def example_upload_price_list(distributor_id, file_or_path):
-    # В данном методе аргуемент file_or_path может принимать как путь к файлу так и откытый файл.
+async def example_upload_price_list(distributor_id, upload_file):
+    # В данном методе аргуемент upload_file может принимать как путь к файлу так и откытый файл.
     # Открытый файл будет закрыт еще до отправки запроса к API
-    data = await api.cp.admin.distributors.pricelist_update(distributor_id=distributor_id, file_or_path=file_or_path)
+    data = await api.cp.admin.distributors.pricelist_update(distributor_id=distributor_id, upload_file=upload_file)
     logger.info(data)
 
 
@@ -20,7 +20,8 @@ if __name__ == '__main__':
     files = os.listdir('files')
     distributors = [1642117]
     for i in range(len(files)):
-        asyncio.run(example_upload_price_list(distributor_id=distributors[i],
-                                              file_or_path=open(f'{cwd}/files/{files[i]}', 'rb')))
-        # asyncio.run(example_upload_price_list(distributor_id=1642117,
-        #                                       file_or_path=f'{cwd}/files/{files[i]}'))
+        asyncio.run(example_upload_price_list(distributor_id=1642117,
+                                              upload_file=f'{cwd}/files/{files[i]}'))
+        # asyncio.run(example_upload_price_list(distributor_id=distributors[i],
+        #                                       upload_file=open(f'{cwd}/files/{files[i]}', 'rb')))
+
