5eb53b2ae1a1ed126847fe65161d906dd8aa7c68
bl4ckm45k <nonpowa@gmail.com>
2023-04-16 19:57:36 +0300

    Changing the type of config file using dataclasses

--- examples/config.py
+++ examples/config.py
@@ -1,11 +1,26 @@
+from dataclasses import dataclass
+
 from environs import Env
 
-env = Env()
-env.read_env()
 
-host = env.str('host')
-login = env.str('login')
-password = env.str('password')
-guest_id = env.str('guest_id')
-login_user = env.str('login_user')
-password_user = env.str('password_user')
+@dataclass
+class Abcp:
+    host: str
+    login: str
+    password: str
+
+
+@dataclass
+class Config:
+    abcp: Abcp
+
+
+def load_config(path: str = None):
+    env = Env()
+    env.read_env(path)
+
+    return Config(
+        abcp=Abcp(
+            host=env.str('ABCP_HOST'),
+            login=env.str('ABCP_LOGIN'), password=env.str('ABCP_PASSWORD')
+        ))
