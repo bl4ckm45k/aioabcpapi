7e83f58afca4dca44d73cfa0bae4ac280f3d0aeb
bl4ckm45k <nonpowa@gmail.com>
2023-07-23 15:26:52 +0300

    bump python, aiohttp, ujson, certifi

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import sys
 from setuptools import setup
 
-if sys.version_info < (3, 7):
+if sys.version_info < (3, 8):
     raise RuntimeError('Your Python version {0} is not supported, please install '
-                       'Python 3.7+'.format('.'.join(map(str, sys.version_info[:3]))))
-requirements = ["aiohttp>=3.8.3", "certifi>=2022.12.7", "ujson>=5.7.0", "pytz>=2022.7.1", "pyrfc3339"]
+                       'Python 3.8+'.format('.'.join(map(str, sys.version_info[:3]))))
+requirements = ["wheel", "aiohttp>=3.8.5", "certifi>=2023.7.22", "ujson>=5.8.0", "pytz>=2023.3", "pyrfc3339"]
 setup(
     name='aioabcpapi',
-    version='2.0.6',
+    version='2.0.7',
     author='bl4ckm45k',
     author_email='nonpowa@gmail.com',
     description='Async library for ABCP API',
@@ -17,7 +17,6 @@ setup(
     packages=['aioabcpapi', 'aioabcpapi/cp', 'aioabcpapi/ts', 'aioabcpapi/utils'],
     install_requires=requirements,
     classifiers=[
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
@@ -29,5 +28,5 @@ setup(
         'Topic :: Software Development :: Libraries :: Application Frameworks',
 
     ],
-    python_requires='>=3.7'
+    python_requires='>=3.8'
 )
