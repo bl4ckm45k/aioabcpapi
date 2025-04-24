1b6e157e28ea58e0ebda16c0d6c50dc5ca6c674f
Daniil Ryzhov <nonpowa@gmail.com>
2025-04-25 01:44:33 +0300

    Optimize imports, bump Python version to 3.10+

--- aioabcpapi/__init__.py
+++ aioabcpapi/__init__.py
@@ -1,12 +1,13 @@
 import sys
-from .base import BaseAbcp
+
 from .abcp import Abcp
+from .base import BaseAbcp
 from .exceptions import (NetworkError, UnsupportedHost, UnsupportedLogin, PasswordType, NotEnoughRights, AbcpAPIError,
                          AbcpParameterRequired, TeaPot, AbcpNotFoundError, FileSizeExceeded, AbcpWrongParameterError)
 
-if sys.version_info < (3, 8):
+if sys.version_info < (3, 10):
     raise RuntimeError('Your Python version {0} is not supported, please install '
-                       'Python 3.8+'.format('.'.join(map(str, sys.version_info[:3]))))
+                       'Python 3.10+'.format('.'.join(map(str, sys.version_info[:3]))))
 
 __author__ = 'bl4ckm45k'
 __version__ = '2.2.0'
